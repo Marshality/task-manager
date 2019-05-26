704b9f08ec6cabf9d1dca5d33e6e9486c72d8d5f
Marshality <marshalityy@gmail.com>
2019-05-26 19:08:51 +0300

    Удален ненужный функционал

---
+++ main/web-server/beastserver/RequestHandler.h
@@ -0,0 +1,14 @@
+//
+// Created by Marseille Gulaya on 2019-05-26.
+//
+
+#ifndef MAIN_REQUESTHANDLER_H
+#define MAIN_REQUESTHANDLER_H
+
+
+class RequestHandler {
+
+};
+
+
+#endif //MAIN_REQUESTHANDLER_H
